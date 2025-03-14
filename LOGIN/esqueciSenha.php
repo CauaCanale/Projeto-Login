8d6eb3081423102af9dac346a778f4ce05fa1ae0
CauaCanale <caua.cferreira2@gmail.com>
2025-03-14 11:58:50 -0300

    Add files via upload

---
+++ LOGIN/esqueciSenha.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esqueci Senha</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<style>
+    body {
+        display: flex;
+        justify-content: center;
+        background-color: #0e0e0e;
+        color: #f2f2f2;
+    }
+
+    form {
+        width: 500px;
+    }
+
+    button[type="submit"] {
+        margin-top: 20px;  
+    }
+</style>
+
+<body>
+    <form action="  ">
+        <h1>Esqueci a Senha</h1>
+
+        <label class="form-label"></label>
+        <input type="text" placeholder="Usuario:" class="form-control">
+
+        <label class="form-label"></label>
+        <input type="number" placeholder="CPF:" class="form-control">
+
+        <button type="submit" class="btn btn-success">Enviar</button>
+
+    </form>
+
+
+</body>
+
+</html>
